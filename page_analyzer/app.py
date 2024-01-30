8ea8c53a81c295f22551daf82f48c999510542e7
AnastasiaTimoshe <a.n.timoshevskaya@gmail.com>
2024-01-30 18:05:17 +0100

    step 9 from part 1 add app.py

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,4 +1,9 @@
+import os
 from flask import Flask
+from dotenv import load_dotenv
 
+
+load_dotenv()  # take environment variables from .env.
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
