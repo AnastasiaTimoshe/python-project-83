70b352acb9e6fa74b1f748e754a24b8a08a6440e
AnastasiaTimoshe <a.n.timoshevskaya@gmail.com>
2024-01-31 11:38:59 +0100

    add/update templates/components

--- Makefile
+++ Makefile
@@ -1,12 +1,6 @@
 install:
 	poetry install
 
-package-install:
-	python3 -m pip install --user dist/hexlet_code-0.1.0-py3-none-any.whl
-
-package-reinstall:
-	python3 -m pip install . --force-reinstall
-
 lint:
 	poetry run flake8
 
--- database.sql
+++ database.sql
@@ -1,18 +1,18 @@
-DROP TABLE IF EXISTS url_checks;
-DROP TABLE IF EXISTS urls;
-
-CREATE TABLE urls (
-	id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-	name varchar(255) UNIQUE NOT NULL,
-	created_at date NOT NULL
+CREATE TABLE urls(
+    id  bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name  varchar(255) UNIQUE,
+    created_at DATE DEFAULT CURRENT_DATE
 );
 
-CREATE TABLE url_checks (
-	id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-	url_id bigint REFERENCES urls (id) ON DELETE CASCADE NOT NULL,
-	status_code integer,
-	h1 varchar(255),
-	title varchar(255),
-	description varchar(255),
-	created_at date NOT NULL
+CREATE TABLE url_checks(
+    id  bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    url_id  bigint,
+    status_code  integer,
+    h1  varchar(255),
+    title  varchar(255),
+    description  varchar(255),
+    created_at DATE DEFAULT CURRENT_DATE,
+    CONSTRAINT fk_url
+      FOREIGN KEY(url_id)
+	  REFERENCES urls(id)
 );
