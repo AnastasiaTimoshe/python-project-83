79c7ed331c490a433a8772fbe71d7823c1881e0f
AnastasiaTimoshe <a.n.timoshevskaya@gmail.com>
2024-01-31 21:45:49 +0100

    update db

--- database.sql
+++ database.sql
@@ -1,15 +1,18 @@
-CREATE TABLE IF NOT EXISTS urls (
-    id SERIAL PRIMARY KEY,
-    name varchar(255) UNIQUE NOT NULL,
-    created_at date
+CREATE TABLE urls(
+    id  bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name  varchar(255) UNIQUE,
+    created_at DATE DEFAULT CURRENT_DATE
 );
 
-CREATE TABLE IF NOT EXISTS url_checks (
-    id SERIAL PRIMARY KEY,
-    url_id bigint REFERENCES urls (id),
-    status_code integer,
-    h1 text,
-    title text,
-    description text,
-    created_at date
+CREATE TABLE url_checks(
+    id  bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    url_id  bigint,
+    status_code  integer,
+    h1  varchar(255),
+    title  varchar(255),
+    description  varchar(255),
+    created_at DATE DEFAULT CURRENT_DATE,
+    CONSTRAINT fk_url
+      FOREIGN KEY(url_id)
+	  REFERENCES urls(id)
 );
